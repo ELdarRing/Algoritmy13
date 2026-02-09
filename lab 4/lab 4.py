1f0c742afa5a23b76907640895c45a99e2b2f795
ELdarRing <eldarablyazov123@gmail.com>
2026-02-09 15:58:45 +0500

    dop

--- lab 4/lab 4.py
+++ lab 4/lab 4.py
@@ -30,3 +30,36 @@ i = 1
 while i <= 10:
     print(i)
 # цикл является бесконечным, потому что после выполнения условия с i не выполняются действия
+
+# Доп задание
+all_count = 0
+positive_count = 0
+negative_count = 0
+all_sum = 0
+
+print("Введите числа:")
+
+while True:
+    num = int(input())
+    if num == 0:
+        break
+
+    all_count += 1
+    all_sum += num
+
+    if num > 0:
+        positive_count += 1
+    elif num < 0:
+        negative_count += 1
+
+print("Общее количество чисел:", all_count)
+print("Положительных чисел:", positive_count)
+print("Отрицательных чисел:", negative_count)
+print("Сумма чисел:", all_sum)
+
+if all_count > 0:
+    print("Среднее арифметическое:", all_sum / all_count)
+else:
+    print("Среднее арифметическое не вычисляется")
+
+
