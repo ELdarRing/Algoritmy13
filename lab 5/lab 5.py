fff7e5c996b67728a8095ff70f1fe11bd204a779
ELdarRing <eldarablyazov123@gmail.com>
2026-02-16 14:16:48 +0500

    1

---
+++ lab 5/lab 5.py
@@ -0,0 +1,14 @@
+#Задание 1
+n = int(input())
+numbers = []
+
+for i in range(n):
+    value = int(input())
+    numbers.append(value)
+
+print(numbers)
+
+#Задание 2
+#Задание 3
+#Задание 4
+#Задание 5
